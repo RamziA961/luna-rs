d4618dfba37401947539fea853f69c304ce95688
RamziA961 <ramziachahine@pm.me>
2026-05-24 22:45:44 +0100

    Simplify configuration code

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,13 +1,14 @@
 use config::{Config, File};
 
-const RELEASE_SOURCE: &str = "Secrets.toml";
-const DEV_SOURCE: &str = "Secrets.dev.toml";
+#[cfg(not(debug_assertions))]
+const CONFIG_SORUCE: &str = "Secrets.toml";
+#[cfg(debug_assertions)]
+const CONFIG_SORUCE: &str = "Secrets.dev.toml";
 
 #[derive(Debug, Clone)]
 pub struct ConfigurationVariables {
     discord_token: String,
     youtube_api_key: String,
-    gemini_api_key: String,
     #[cfg(debug_assertions)]
     dev_guild_id: usize,
 }
@@ -19,41 +20,13 @@ impl Default for ConfigurationVariables {
 }
 
 impl ConfigurationVariables {
-    #[cfg(not(debug_assertions))]
-    pub fn new() -> Self {
-        let vars = Config::builder()
-            .add_source(File::with_name(RELEASE_SOURCE))
-            .build()
-            .expect(&format!(
-                "Configuration file not found. {RELEASE_SOURCE} file expected."
-            ));
-
-        let discord_token = vars
-            .get_string("DISCORD_TOKEN")
-            .expect("Expected DISCORD_TOKEN.");
-
-        let youtube_api_key = vars
-            .get_string("YOUTUBE_API_KEY")
-            .expect("Expected YOUTUBE_API_KEY.");
-
-        let gemini_api_key = vars
-            .get_string("GEMINI_API_KEY")
-            .expect("Expected GEMINI_API_KEY.");
-
-        Self {
-            discord_token,
-            youtube_api_key,
-            gemini_api_key,
-        }
-    }
-
     #[cfg(debug_assertions)]
     pub fn new() -> Self {
         let vars = Config::builder()
-            .add_source(File::with_name(DEV_SOURCE))
+            .add_source(File::with_name(CONFIG_SORUCE))
             .build()
             .unwrap_or_else(|_| {
-                panic!("Configuration file not found. {RELEASE_SOURCE} file expected.")
+                panic!("Configuration file not found. {CONFIG_SORUCE} file expected.")
             });
 
         let discord_token = vars
@@ -64,17 +37,14 @@ impl ConfigurationVariables {
             .get_string("YOUTUBE_API_KEY")
             .expect("Expected YOUTUBE_API_KEY.");
 
-        let gemini_api_key = vars
-            .get_string("GEMINI_API_KEY")
-            .expect("Expected GEMINI_API_KEY.");
-
+        #[cfg(debug_assertions)]
         let dev_guild_id = vars.get::<usize>("GUILD_ID").expect("Expected GUILD_ID.");
 
         Self {
             discord_token,
             youtube_api_key,
+            #[cfg(debug_assertions)]
             dev_guild_id,
-            gemini_api_key,
         }
     }
 
@@ -84,10 +54,6 @@ impl ConfigurationVariables {
 
     pub fn youtube_api_key(&self) -> &str {
         &self.youtube_api_key
-    }
-
-    pub fn gemini_api_key(&self) -> &str {
-        &self.gemini_api_key
     }
 
     #[cfg(debug_assertions)]
